5410b7067419e62da29485480a007fb7c8c9d3aa
Alejandrojuarez0105 <alejandrojuarez0105@gmail.com>
2024-06-11 20:41:07 +0200

    Agregue retos

---
+++ TICTACTOE/TICTACTOE/src/Jugador.java
@@ -0,0 +1,36 @@
+
+public class Jugador {
+
+    private char ficha;
+
+    public Jugador(char ficha) {
+        this.ficha = ficha;
+    }
+
+    public char ficha() {
+        return ficha;
+    }
+
+    public void celebrar() {
+        System.out.println("Los " + ficha + " han ganado!!!!");
+    }
+
+    public void ponerFicha(Tablero tablero) {
+        Coordenada coordenada = new Coordenada();
+        do {
+            coordenada.recoger();
+        } while (!coordenada.valida() || tablero.ocupado(coordenada));
+
+        tablero.ponerFicha(coordenada, ficha);
+    }
+
+    public void moverFicha(Tablero tablero) {
+        Coordenada coordenada = new Coordenada();
+        do {
+            coordenada.recoger();
+        } while (!coordenada.valida() || !tablero.ocupado(coordenada));
+        tablero.sacarFicha(coordenada);
+        ponerFicha(tablero);
+    }
+
+}
